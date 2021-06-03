5cdaf4fbbba27480103b58b5ec19d39fb581ee2d
Guilherme Cruz <71985319+guilhermehccruz@users.noreply.github.com>
2021-06-03 19:18:57 -0300

    Nodemon and orm changes

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,35 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import {Request, Response} from "express";
-import Routes from "./routes";
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { Request, Response } from 'express';
+import Routes from './routes';
 
-createConnection().then(connection =>{
-    
-    const app = express();
-    app.use(bodyParser.json());
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, route['middleware'](route.action_key) , (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next);
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
-            } else if (result !== null && result !== undefined) {
-                res.json(result);
-            }
-        });
-    });
-app.listen(3000);
+createConnection().then(connection => {
+	const app = express();
+	app.use(bodyParser.json());
+	// register express routes from defined application routes
+	Routes.forEach(route => {
+		(app as any)[route.method](
+			route.route,
+			route['middleware'](route.action_key),
+			(req: Request, res: Response, next: Function) => {
+				const result = new (route.controller as any)()[route.action](
+					req,
+					res,
+					next
+				);
+				if (result instanceof Promise) {
+					result.then(result =>
+						result !== null && result !== undefined
+							? res.send(result)
+							: undefined
+					);
+				} else if (result !== null && result !== undefined) {
+					res.json(result);
+				}
+			}
+		);
+	});
+	app.listen(3000, () => console.log(`Server started on port 3000`));
 });
-
-
-
-
